463ce363aed8f1c45eb65d4843bcf02c2cf7297f
Ruchintha <rucinthaw@gmail.com>
2019-03-06 17:14:53 +0530

    new preloader

--- resources/js/main.js
+++ resources/js/main.js
@@ -222,5 +222,5 @@ $(document).on('ready', function() {
         // will first fade out the loading animation
     jQuery("#status").fadeOut();
         // will fade out the whole DIV that covers the website.
-    jQuery("#preloader").delay(1000).fadeOut("slow");
+    jQuery("#sitepreloader").delay(1000).fadeOut("slow");
 })
